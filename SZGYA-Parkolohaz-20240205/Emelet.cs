cbd9c1a736676f93e569812389bf2f1ab89b1d88
Csizmadia Márk <airamek@teamorange.hu>
2024-02-05 14:05:39 +0100

    Add project files.

---
+++ SZGYA-Parkolohaz-20240205/Emelet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZGYA_Parkolohaz_20240205
+{
+    internal class Emelet
+    {
+        public int Szint { get; set; }
+        public string Nev { get; set; }
+        public List<int> Szektorok { get; set; }
+
+        public Emelet(int szint, string sor) 
+        {
+            this.Szint = szint;
+            this.Szektorok = new List<int>();
+            string[] adatok = sor.Split("; ");
+            this.Nev = adatok[0];
+            for (int i = 1; i < adatok.Length; i++)
+            {
+                this.Szektorok.Add(int.Parse(adatok[i]));
+            }
+        }
+
+        public override string ToString()
+        {
+            string str = $"{this.Szint,2}.szint|{Nev, 8}";
+            foreach (var sz in this.Szektorok)
+            {
+                str += $"|{sz, 2}";
+            }
+            return str;
+        }
+    }
+}
---
+++ SZGYA-Parkolohaz-20240205/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace SZGYA_Parkolohaz_20240205
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Emelet> emeletek = new List<Emelet>();
+
+            var sr = new StreamReader("../../../src/parkolohaz.txt", Encoding.UTF8);
+            for (int i = 1; !sr.EndOfStream; i++)
+            {
+                emeletek.Add(new Emelet(i, sr.ReadLine()));
+            }
+            sr.Close();
+
+            foreach (var emelet in emeletek)
+            {
+                Console.WriteLine(emelet);
+            }
+
+            //8 
+            Console.WriteLine($"\n8.feladat {emeletek.MinBy(e => e.Szektorok.Sum()).Nev}");
+
+            //9
+            Console.WriteLine($"\n9.feladat: {emeletek.FirstOrDefault(e => e.Szektorok.Contains(0))}");
+        }
+    }
+}
